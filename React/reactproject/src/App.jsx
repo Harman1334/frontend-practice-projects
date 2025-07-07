f4d86fb9ef167b11cdcf81f78a17b900c56544d4
Harman1334 <harmanpaul414@gmail.com>
2025-07-07 18:55:57 +0530

    react setup

---
+++ React/reactproject/src/App.jsx
@@ -0,0 +1,21 @@
+/*import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'*/
+
+import Intro from "./component/intro"
+
+function App() {
+  return (
+    <>
+  {/*fragment*/}
+    <Intro/>
+    <Intro/>
+    <Intro/>
+    </>
+  )
+  
+      
+}
+
+export default App
---
+++ React/reactproject/src/component/Intro.jsx
@@ -0,0 +1,11 @@
+function Intro(){
+    return(
+        <ul>
+            <li>hello..!!</li>
+            <li>My name is Harman</li>
+            <li>I am a student.</li>
+            <hr/>
+        </ul>
+    )
+}
+export default Intro
